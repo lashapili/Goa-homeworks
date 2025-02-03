747665db953b6c92d00da68c975dbf6aeb9da86c
lashapili <lashaphili.7@gmail.com>
2025-02-03 21:19:09 +0400

    second commit

---
+++ level 18/classwork/phyton.py
@@ -0,0 +1,15 @@
+for i in range(10):
+    print(i)
+for i in range(50,150):
+    print(i)
+    
+    print(i)
+for i in range(0,50,1):
+    print(i)
+start=int(input("enter number1: "))
+end=int(input("enter number2: "))
+for i in range(start,end):
+    print(i)
+name=input("enter your name")
+for i in name:
+    print(i)
---
+++ level 18/homework/phyton.py
@@ -0,0 +1,14 @@
+name="lasha"
+surename="philishvili"
+for i in range(9):
+    print(name)
+for i in range(9):
+    print(surename)
+for i in range(1,20):
+    print(i)
+for i in range(20,0,-1):
+    print(i)
+for i in range(50):
+    print(i)
+for i in range(1,10,2):
+    print(i)

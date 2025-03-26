2da4fbf27b84267529333144f72b4250949b74c9
lashapili <lashaphili.7@gmail.com>
2025-03-26 19:48:17 +0400

    second commit

---
+++ level 24/classwork/phyton.py
@@ -0,0 +1,7 @@
+full_number=int(input("enter full number: "))
+if full_number>0:
+    print("the number is positive")
+elif full_number<0:
+    print("the number is negative")
+else:
+    print("the number is zero")
---
+++ level 24/classwork/phyton1.py
@@ -0,0 +1,6 @@
+my_password=2018
+try_password=int(input("enter correct password: "))
+if try_password == my_password:
+    print("correct password")
+else:
+    print("incorrect password")

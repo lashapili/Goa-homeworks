9b52f177a06945d6db54ff839616cd8ef5592c4e
lashapili <lashaphili.7@gmail.com>
2025-04-08 18:42:14 +0400

    second commit

---
+++ level 26/classwork/phyton2.py
@@ -0,0 +1,4 @@
+sentence = "Practice makes perfect"
+reversed_sentence = sentence[::-1]
+print( sentence)
+print( reversed_sentence)
---
+++ level 26/homework/phyton.py
@@ -0,0 +1,10 @@
+number=["1","2","3","4","5","6","7","8","9","10"]
+print(number[slice (5)])
+
+
+number1=["20","21","22","23","24","25","26","27","28","29","30"]
+print(number1[slice(30)])
+
+
+goa="Goal-Oriented Academy"
+print(goa[slice(14,21)])
